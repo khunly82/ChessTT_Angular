fc2170223a1c7ad1588fb288c5bdc442e931f60a
Khun <lykhun@gmail.com>
2025-03-31 14:57:22 +0200

    change env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://khun.somee.com/api',
+  apiBaseUrl: 'https://chessapikhun-d8f0cnahf7c5a2dj.westeurope-01.azurewebsites.net/api',
 };
