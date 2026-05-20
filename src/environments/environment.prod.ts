793938caa08cd7e612e1cb0eca5092ebdb9842b6
Khun <khunly@bstorm.be>
2026-05-20 12:33:22 +0200

    fix: update production API base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://khun-chess-api-draya2ebc8bqdhfv.westeurope-01.azurewebsites.net/api',
+  apiBaseUrl: 'https://tfchessapi-dmewhhepbuccc5ae.westeurope-01.azurewebsites.net/api',
 };
